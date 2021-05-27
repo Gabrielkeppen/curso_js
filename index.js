0d2f9ba4f0abd2ff5017370aed4cc9806c6bef46
gabrielkeppen <gabrielkeppen@gmail.com>
2021-05-27 18:00:07 -0300

    1

---
+++ index.js
@@ -0,0 +1,26 @@
+var olaMundo = ("ola mundo")
+console.log(olaMundo);
+
+let a = 10 
+const b = "10"
+console.log(a == b && typeof a == 'string')
+
+let cor = ""
+
+if (cor === "verde"){
+
+    console.log("siga");
+
+}
+    else if (cor === "amarelo") {
+
+        console.log("atenção")
+    }
+
+    else (cor === "vermelho") {
+
+        console.log("pare")
+
+    }
+
+
