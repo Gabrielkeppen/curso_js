9b34a48971e5423b7740a32498316e297022ef37
gabrielkeppen <gabrielkeppen@gmail.com>
2021-05-28 14:40:29 -0300

    primeiro video completo

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//escrevendo no console
 var olaMundo = ("ola mundo")
 console.log(olaMundo);
 
+
+// determinando constantes e perguntando igualdades
 let a = 10 
 const b = "10"
 console.log(a == b && typeof a == 'string')
 
+
+//fazer o console identificar constante
 let cor = ""
 
 if (cor === "verde"){
@@ -17,10 +22,31 @@ if (cor === "verde"){
         console.log("atenção")
     }
 
-    else (cor === "vermelho") {
+    else if (cor === "vermelho") {
 
         console.log("pare")
 
     }
+    else {
+        console.log("não sei")
+    }
 
 
+//segundo caminho para executar a mesma coisa
+switch (cor) {
+
+    case "verde":
+        console.log('siga');
+        break;
+
+    case "amarelo":
+        console.log('atenção')
+        break;
+
+    case "vermelho":
+        console.log('pare')
+
+    default:
+        console.log("não sei");
+
+}
