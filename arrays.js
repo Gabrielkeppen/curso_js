d39971ee859aa4f8934c364eaf1bec5ca2a4827c
gabrielkeppen <gabrielkeppen@gmail.com>
2021-06-08 14:43:02 -0300

    arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
 let carros = ["WRX", "Smart conversivel", 10, true, new Date(), function(){}];
 
 console.log(carros[1])
+
+//para objeto encontrado dentro de "carros" mostrar em orderm seu index e valor
+carros.forEach(function(value, index){
+
+    console.log(index, value);
+
+})
+
+let meuArray = ["banana","maça","uva","cereja","goiaba"]
+
+meuArray.forEach(function(value,index){
+
+    console.log(index, value);
+
+})
