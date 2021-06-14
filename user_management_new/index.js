e7fd8a6c06d30b8fe9d228b8ff1bd87d782112c9
gabrielkeppen <gabrielkeppen@gmail.com>
2021-06-14 17:36:03 -0300

    if.else

--- user_management_new/index.js
+++ user_management_new/index.js
@@ -1,9 +1,9 @@
+//criando variavel atribuida a um id 
 var nome = document.querySelector("#exampleInputName");
 
 nome.value = "Gabriel"
 
-
-
+//definindo todas variaveis 
 var name = document.querySelector("#exampleInputName");
 var gender = document.querySelector("#form-user-create [name=gender]:checked");
 var birth = document.querySelector("#exampleInputBirth");
@@ -13,13 +13,25 @@ var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
 
-
-
-
+//usando o foreach para todas variaveis
 var fields = document.querySelectorAll("#form-user-create [name]");
 
 fields.forEach(function(field, index){
 
-console.log(field);
+    if (field.nome == "gender"){
+
+        if (field.checked) {
+
+            console.log("sim", field);
+
+        };
+        
+    } else {
+
+        console.log("nao")
+
+    }
+
+    //console.log(field);
 
 });
