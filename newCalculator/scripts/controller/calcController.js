e861d3266c357b402478cab41d5aab26c541239f
gabrielkeppen <gabrielkeppen@gmail.com>
2021-06-17 17:57:53 -0300

    calcController continuado

--- newCalculator/scripts/controller/calcController.js
+++ newCalculator/scripts/controller/calcController.js
@@ -4,6 +4,19 @@ class CalcController {
 
         this._displayCalc = "0";
         this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize() {
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "01/01/2021";
+        timeEl.innerHTML = "20:30";
 
     }
 
